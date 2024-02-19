50107aaaf8a40bec62f6fd2f88321c85d3b967ef
ripperino <vadik.volkorezov@yandex.ru>
2024-02-20 00:26:03 +1000

    Completed day5

--- src/main/java/day5/Task1.java
+++ src/main/java/day5/Task1.java
@@ -1,7 +1,26 @@
-package day5;
+package JavaMarathone2020.JavaMarathon2020.src.main.java.day5;
 
 public class Task1 {
     public static void main(String[] args) {
+        Car car = new Car();
+        Car car1 = new Car();
 
+        car.setYear(1993);
+        car.setColor("White");
+        car.setModel("Carina");
+
+        car1.setYear(1991);
+        car1.setColor("Brown");
+        car1.setModel("Crown");
+
+        System.out.println(car.getYear());
+        System.out.println(car.getColor());
+        System.out.println(car.getModel());
+
+        System.out.println();
+
+        System.out.println(car1.getYear());
+        System.out.println(car1.getColor());
+        System.out.println(car1.getModel());
     }
 }
--- src/main/java/day5/Task2.java
+++ src/main/java/day5/Task2.java
@@ -1,8 +1,16 @@
-package day5;
+package JavaMarathone2020.JavaMarathon2020.src.main.java.day5;
 
 
 public class Task2 {
     public static void main(String[] args) {
+        Motorbike motorbike1 = new Motorbike(1996, "Red", "Ducati");
+        Motorbike motorbike2 = new Motorbike(1992, "Black", "Harley Davidson");
+        Motorbike motorbike3 = new Motorbike(1960, "Green", "Ural");
+        Motorbike motorbike4 = new Motorbike(1938, "Blue", "BMW");
 
+        System.out.println("Motorbike1: " + motorbike1.getYear() + " " + motorbike1.getColor() + " " + motorbike1.getModel());
+        System.out.println("Motorbike2: " + motorbike2.getYear() + " " + motorbike2.getColor() + " " + motorbike2.getModel());
+        System.out.println("Motorbike3: " + motorbike3.getYear() + " " + motorbike3.getColor() + " " + motorbike3.getModel());
+        System.out.println("Motorbike4: " + motorbike4.getYear() + " " + motorbike4.getColor() + " " + motorbike4.getModel());
     }
 }
