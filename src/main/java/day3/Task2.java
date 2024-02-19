ff1b26fc50045b567e7a399d126b78fb4a8d9d25
ripperino <vadik.volkorezov@yandex.ru>
2024-02-19 19:59:13 +1000

    Completed day3

--- src/main/java/day3/Task2.java
+++ src/main/java/day3/Task2.java
@@ -1,7 +1,19 @@
-package day3;
+package JavaMarathone2020.JavaMarathon2020.src.main.java.day3;
+
+import java.util.Scanner;
 
 public class Task2 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        double a = scanner.nextDouble();
+        double b = scanner.nextDouble();
+
+        while (b != 0) {
+            System.out.println(a / b);
+
+            a = scanner.nextDouble();
+            b = scanner.nextDouble();
+        }
     }
 }
--- src/main/java/day3/Task3.java
+++ src/main/java/day3/Task3.java
@@ -1,7 +1,27 @@
-package day3;
+package JavaMarathone2020.JavaMarathon2020.src.main.java.day3;
+
+import java.util.Scanner;
 
 public class Task3 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        double a;
+        double b;
+        int counter = 0;
+
+        while (counter < 5) {
+            a = scanner.nextDouble();
+            b = scanner.nextDouble();
+
+            if (b == 0) {
+                System.out.println("Деление на 0");
+                continue;
+            }
+
+            System.out.println(a / b);
+
+            counter++;
+        }
     }
 }
