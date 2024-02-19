453aa69012badcee7068341a0de2a3b17fd2dbe5
ripperino <vadik.volkorezov@yandex.ru>
2024-02-19 14:59:50 +1000

    Completed day2

--- src/main/java/day2/Task1.java
+++ src/main/java/day2/Task1.java
@@ -1,7 +1,19 @@
-package day2;
+package JavaMarathone2020.JavaMarathon2020.src.main.java.day2;
+
+import java.util.Scanner;
 
 public class Task1 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int level = scanner.nextInt();
 
+        if (level > 0) {
+            if (level <= 4) System.out.println("Малоэтажный дом");
+            if (level >= 5 && level <= 8) System.out.println("Среднеэтажный дом");
+            if (level >= 9) System.out.println("Многоэтажный дом");
+        }
+        else {
+            System.out.println("Ошибка ввода");
+        }
     }
 }
--- src/main/java/day2/Task3.java
+++ src/main/java/day2/Task3.java
@@ -1,7 +1,23 @@
-package day2;
+package JavaMarathone2020.JavaMarathon2020.src.main.java.day2;
+
+import java.util.Scanner;
 
 public class Task3 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        int a = scanner.nextInt() + 1;
+        int b = scanner.nextInt() - 1;
+
+        if (a >= b) {
+            System.out.println("Некорректный ввод");
+        } else {
+            while (a <= b) {
+                if (a % 5 == 0 && a % 10 != 0) {
+                    System.out.print(a + " ");
+                }
+                a++;
+            }
+        }
     }
 }
